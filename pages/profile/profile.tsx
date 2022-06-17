fbbf098ce093be0816754a61f3374023dc1f8f06
tanukizzan <51444253+tanukizzan@users.noreply.github.com>
2022-06-17 09:13:39 +0900

    profileのnavの見た目とエラー修正

--- pages/profile/profile.tsx
+++ pages/profile/profile.tsx
@@ -36,13 +36,13 @@ export default function Profile() {
       <h2 className="home-h2">
         I'm tanukizzan.
       </h2>
-      <p className="paragraph">ブロガー的なプログラマー的な人です。</p>
+      <p className="paragraph">たぬきっつぁんです。ブロガー的なプログラマー的な人です。</p>
       <p className="paragraph">Skills: HTML / CSS / JavaScript / Next.js</p>
       <nav className="flex flex-wrap items-center m-auto lg:m-0">
         {
           linkBtn.map((link: Link) => (
-            <a href={link.url} className="paragraph text-blue-500 hover:underline">{link.title}</a>
-          )).reduce((prev: JSX.Element, current: JSX.Element): any => [prev, (<span className="paragraph mx-2">/</span>), current])
+            <a href={link.url} className="paragraph text-blue-500 hover:underline inline-block relative pr-8 last:pr-0 last-of-type:before:hidden before:absolute before:top-1/2 before:right-3 before:-translate-y-1/2 before:w-1 before:h-1 before:bg-gray-300 before:rounded-full" key={link.title}>{link.title}</a>
+          ))
         }
       </nav>
     </div>

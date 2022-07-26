e2c37efd5aaee868d41ad9a98071117f64b4dbcf
tanukizzan <51444253+tanukizzan@users.noreply.github.com>
2022-07-26 22:13:14 +0900

    fix footer css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,16 +6,16 @@ export default function Footer() {
       <div className="mx-auto my-8 flex flex-wrap flex-col md:flex-row items-center justify-around">
         <Link href={'/'} passHref>
           <a className="flex items-center text-base">
-            <span className="text-lg">Home</span>
+            <span className="text-lg my-4">Home</span>
           </a>
         </Link>
         <Link href={'/privacy-policy'} passHref>
           <a className="flex items-center text-base">
-            <span className="text-lg">Privacy Policy</span>
+            <span className="text-lg my-4">Privacy Policy</span>
           </a>
         </Link>
         <nav className="flex items-center text-base">
-          <p className="text-lg">&copy; 2022 tanukizzan</p>
+          <p className="text-lg my-4">&copy; 2022 tanukizzan</p>
         </nav>
       </div>
     </footer>
